b120722cffefa9b8bb808dc1f703edabaddc8e97
kazurayam <kazuaki.urayama@gmail.com>
2026-07-05 20:49:27 +0900

    update the docs

--- packages/my-app/src/main.tsx
+++ packages/my-app/src/main.tsx
@@ -3,6 +3,7 @@ import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
 import { serveStatic } from '@hono/node-server/serve-static';
 import { Top } from './top';
+import { Section3 } from './section3';
 
 const app = new Hono();
 app.use('*', serveStatic({ root: './static'}))
@@ -13,6 +14,30 @@ app.get('/', (c) => {
         <Top messages={ messages } />
     )
 });
+
+app.get('/section3', (c) => {
+    const messages = ['Hello htmx']
+    return c.render(
+        <Section3 messages={messages} />
+    )
+})
+
+app.on(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], '/hello', (c) => {
+    if (c.req.method == "GET") {
+        return c.render(<span style='color:#ff0000;'>GETリクエスト!</span>)
+    } else if (c.req.method == "POST") {
+        return c.render(<span style='color:#00bf00;'>POSTリクエスト!</span>)
+    } else if (c.req.method == "PUT") {
+        return c.render(<span style='color:#0000ff;'>PUTリクエスト!</span>)
+    } else if (c.req.method == "PATCH") {
+        return c.render(<span style='color:#ff00ff;'>PATCHリクエスト!</span>)
+    } else if (c.req.method == "DELETE") {
+        return c.render(<span style='color:#ff0000;'>DELETEリクエスト!</span>)
+    } else {
+        throw new Error('unexpected c.req.method=' + c.req.method)
+    }
+})
+
 
 const server = serve({
     port: 3001,
